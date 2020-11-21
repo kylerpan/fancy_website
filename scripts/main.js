01bee7a6e1ab51f5990afbb211a950126a2a8f86
kylerpan <kylerpan691@gmail.com>
2020-11-21 12:25:00 -0800

    adding drop down

---
+++ scripts/main.js
@@ -0,0 +1,11 @@
+/* || Header */
+var gallery = document.getElementsByClassName('gallery')[0];
+var more = document.getElementsByClassName('more')[0];
+
+gallery.onclick = function() {
+    console.log("hi");
+}
+
+more.onclick = function() {
+    console.log("hi");
+}
