b5e1e92bbe8d8d7859fa851a01ec79cb31c01098
kylerpan <kylerpan691@gmail.com>
2020-11-22 08:19:30 -0800

    using js animation instead of css

--- scripts/main.js
+++ scripts/main.js
@@ -1,29 +1,61 @@
 /* || Header */
-var gallery = document.getElementsByClassName('gallery')[0];
-var more = document.getElementsByClassName('more')[0];
-var gallery_drop_down = document.getElementsByClassName('gallery-drop-down')[0];
-var more_drop_down = document.getElementsByClassName('more-drop-down')[0];
+var gallery = document.getElementById('gallery');
+var more = document.getElementById('more');
+var gallery_drop_down = document.getElementById('gallery-drop-down');
+var more_drop_down = document.getElementById('more-drop-down');
 
 gallery.onclick = function() {
     console.log("hi");
 }
 
-gallery.addEventListener("mouseover", function(event) {
-    gallery_drop_down.style.display = "block";
-}, false);
-
-gallery.addEventListener("mouseout", function(event) {
-    gallery_drop_down.style.display = "none";
-}, false);
-
 more.onclick = function() {
     console.log("hi");
 }
 
+gallery.addEventListener("mouseover", function(event) {
+    gallery.innerHTML = "Gallery &and;";
+    
+    var start = Date.now();
+    var timer = setInterval(function() {
+        var timePassed = Date.now() - start;
+        gallery_drop_down.style.top = timePassed / 2 + 'px';
+        gallery_drop_down.style.opacity = timePassed / 1.7 + "%";
+        if (timePassed > 170) clearInterval(timer);
+    }, 20)
+}, false);
+
+gallery.addEventListener("mouseout", function(event) {
+    gallery.innerHTML = "Gallery &or;";
+    
+    var start = Date.now();
+    var timer = setInterval(function() {
+        var timePassed = Date.now() - start;
+        gallery_drop_down.style.top = 85 - timePassed / 2 + 'px';
+        gallery_drop_down.style.opacity = 100 - timePassed / 1.7 + "%";
+        if (timePassed > 170) clearInterval(timer);
+    }, 20)
+}, false);
+
 more.addEventListener("mouseover", function(event) {
-    more_drop_down.style.display = "block";
+    more.innerHTML = "More &and;";
+
+    var start = Date.now();
+    var timer = setInterval(function() {
+        var timePassed = Date.now() - start;
+        more_drop_down.style.top = timePassed / 2 + 'px';
+        more_drop_down.style.opacity = timePassed / 1.7 + "%";
+        if (timePassed > 170) clearInterval(timer);
+    }, 20)
 }, false);
 
 more.addEventListener("mouseout", function(event) {
-    more_drop_down.style.display = "none";
+    more.innerHTML = "More &or;";
+
+    var start = Date.now();
+    var timer = setInterval(function() {
+        var timePassed = Date.now() - start;
+        more_drop_down.style.top = 85 - timePassed / 2 + 'px';
+        more_drop_down.style.opacity = 100 - timePassed / 1.7 + "%";
+        if (timePassed > 170) clearInterval(timer);
+    }, 20)
 }, false);
