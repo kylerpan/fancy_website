1017fc66a1ff951ff0c35927bee4845d2db7da28
kylerpan <kylerpan691@gmail.com>
2020-11-25 10:55:34 -0800

    fixing drop down style

--- scripts/main.js
+++ scripts/main.js
@@ -18,18 +18,24 @@ gallery.addEventListener("mouseover", function(event) {
     var start = Date.now();
     var timer = setInterval(function() {
         var timePassed = Date.now() - start;
-        gallery_drop_down.style.top = timePassed / 3 + 'px';
-        gallery_drop_down.style.opacity = timePassed / 1.7 + "%";
-        if (timePassed > 170) clearInterval(timer);
+        gallery_drop_down.style.top = timePassed / 2 + 'px';
+        gallery_drop_down.style.opacity = timePassed / 1.8 + "%";
+        if (timePassed > 180) {
+            gallery_drop_down.style.top = "90px";
+            gallery_drop_down.style.opacity = "100%";
+            clearInterval(timer);
+        }
     }, 20)
 }, false);
 
-gallery_drop_down.addEventListener("mouseover", function(event) {
-    console.log("hi");
-    gallery.innerHTML = "Gallery &and;";
-    gallery_drop_down.style.top = "85px";
-    gallery_drop_down.style.opacity = "100%";
-}, false)
+if (gallery_drop_down.style.opacity == "100%") {
+    gallery_drop_down.addEventListener("mouseover", function(event) {
+        console.log("hi");
+        gallery.innerHTML = "Gallery &and;";
+        gallery_drop_down.style.top = "85px";
+        gallery_drop_down.style.opacity = "100%";
+    }, false)
+}
 
 gallery.addEventListener("mouseout", function(event) {
     gallery.innerHTML = "Gallery &or;";
@@ -37,9 +43,13 @@ gallery.addEventListener("mouseout", function(event) {
     var start = Date.now();
     var timer = setInterval(function() {
         var timePassed = Date.now() - start;
-        gallery_drop_down.style.top = 85 - timePassed / 2 + 'px';
-        gallery_drop_down.style.opacity = 100 - timePassed / 1.7 + "%";
-        if (timePassed > 170) clearInterval(timer);
+        gallery_drop_down.style.top = 90 - timePassed / 2 + 'px';
+        gallery_drop_down.style.opacity = 100 - timePassed / 1.8 + "%";
+        if (timePassed > 180) {
+            gallery_drop_down.style.top = "90px";
+            gallery_drop_down.style.opacity = "0%";
+            clearInterval(timer);
+        }
     }, 20)
 }, false);
 
@@ -50,8 +60,12 @@ more.addEventListener("mouseover", function(event) {
     var timer = setInterval(function() {
         var timePassed = Date.now() - start;
         more_drop_down.style.top = timePassed / 2 + 'px';
-        more_drop_down.style.opacity = timePassed / 1.7 + "%";
-        if (timePassed > 170) clearInterval(timer);
+        more_drop_down.style.opacity = timePassed / 1.8 + "%";
+        if (timePassed > 180) {
+            more_drop_down.style.top = "90px";
+            more_drop_down.style.opacity = "100%";
+            clearInterval(timer);
+        }
     }, 20)
 }, false);
 
@@ -61,8 +75,12 @@ more.addEventListener("mouseout", function(event) {
     var start = Date.now();
     var timer = setInterval(function() {
         var timePassed = Date.now() - start;
-        more_drop_down.style.top = 85 - timePassed / 2 + 'px';
-        more_drop_down.style.opacity = 100 - timePassed / 1.7 + "%";
-        if (timePassed > 170) clearInterval(timer);
+        more_drop_down.style.top = 90 - timePassed / 2 + 'px';
+        more_drop_down.style.opacity = 100 - timePassed / 1.8 + "%";
+        if (timePassed > 180) {
+            more_drop_down.style.top = "0px";
+            more_drop_down.style.opacity = "0%";
+            clearInterval(timer);
+        }
     }, 20)
 }, false);
