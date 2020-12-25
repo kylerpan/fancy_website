e0582cb0385de1edcaec82e5202197811bebe281
kylerpan <kylerpan691@gmail.com>
2020-12-24 22:26:44 -0800

    copying footer over to welcome and about me

--- scripts/main.js
+++ scripts/main.js
@@ -20,3 +20,37 @@ gallery.onclick = function() {
 more.onclick = function() {
     console.log("hi");
 }
+
+/* || Footer */
+var footer_logo = document.getElementById('footer-logo');
+var footer_about_me = document.getElementById('footer-about-me');
+var footer_gallery = document.getElementById('footer-gallery');
+var footer_portrait = document.getElementById('footer-portrait');
+var footer_landscape = document.getElementById('footer-landscape');
+var footer_more = document.getElementById('footer-more');
+
+footer_logo.onclick = function() {
+    window.scrollTo(0, 0);
+}
+
+footer_about_me.onclick = function() {
+    window.location.href = "/about_me";
+}
+
+footer_gallery.onclick = function() {
+    window.location.href = "/gallery";
+}
+
+footer_portrait.onclick = function() {
+    window.location.href = "/gallery";
+    window.scrollTo(0, 1300);
+}
+
+footer_landscape.onclick = function() {
+    window.location.href = "/gallery";
+    window.scrollTo(0, 2520);
+}
+
+footer_more.onclick = function() {
+    console.log("hi");
+}
